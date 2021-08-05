04e272d41a04e29134901a52236105487858af47
Samuel Kiegeland <kiegeland@cl.uni-heidelberg.de>
2021-08-05 17:38:43 +0200

    added config and log

--- code/utility.py
+++ code/utility.py
@@ -1,5 +1,7 @@
 import pickle
 import pandas as pd 
+import yaml 
+import logging
 
 def load_key(path):
     with open(path, 'r') as f: 
@@ -19,3 +21,37 @@ def load_data(path, is_df=False):
     else:
         with open(path + '.pkl', 'rb') as f:
             return pickle.load(f)
+
+def load_config(path="configs/default.yaml") -> dict:
+    """
+    Loads and parses a YAML configuration file.
+
+    :param path: path to YAML configuration file
+    :return: configuration dictionary
+    """
+    with open(path, 'r') as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+    return cfg
+
+
+def make_logger(log_file: str = None, name='logger'):
+    """
+    Create a logger for logging the training/testing process.
+
+    :param log_file: path to file where log is stored as well
+    :return: logger object
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(level=logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s %(message)s')
+    if log_file is not None:
+        fh = logging.FileHandler(log_file)
+        fh.setLevel(level=logging.DEBUG)
+        logger.addHandler(fh)
+        fh.setFormatter(formatter)
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.INFO)
+    sh.setFormatter(formatter)
+    logging.getLogger("").addHandler(sh)
+    logger.info("Hello! This is TCC")
+    return logger
