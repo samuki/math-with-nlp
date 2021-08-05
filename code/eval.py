cf18f294f09c8afa76a39169720046b653e6055f
Samuel Kiegeland <kiegeland@cl.uni-heidelberg.de>
2021-08-05 23:37:16 +0200

    improved eval

--- code/eval.py
+++ code/eval.py
@@ -2,17 +2,10 @@ import pandas as pd
 from utility import load_data
 
 def calc_accuracy(path, is_df=False):
-    if is_df:
-        results = load_data(path, is_df)
-        return results.loc[results['Prediction'] == results['Ground truth']].shape[0]/\
-            results.shape[0]
-    else: 
-        results = load_data(path)
-        #print(results)
-        results = pd.DataFrame.from_dict(results,orient='index')
-        print(results.shape[0])
-        return results.loc[results['Prediction'] == results['Ground truth']].shape[0]/\
-            results.shape[0]
+    results=load_data(path, is_df)
+    print(results.shape[0])
+    return results.loc[results['Prediction'] == results['Ground truth']].shape[0]/\
+        results.shape[0]
 
 def main():
     path="experiments/arithmetic__add_or_sub"
