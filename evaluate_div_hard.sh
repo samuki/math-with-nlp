7f444761fb98f12cabed15da2fa97a7a68a2574a
samuki <s.kiegeland@gmx.de>
2021-08-16 23:17:11 +0200

    added sh

---
+++ evaluate_div_hard.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --job-name=arithmetic_div
+#SBATCH --output=train_logs/arithmetic_div_out.txt
+#SBATCH --ntasks=1
+#SBATCH --mem=64G
+#SBATCH --partition=single
+#SBATCH --time=2-0:00:00
+#SBATCH -e train_logs/arithmetic_div_error.txt
+# Add ICL-Slurm binaries to path
+PATH=/opt/slurm/bin:"$PATH"
+LD_LIBRARY_PATH=/usr/local/cuda/targets/x86_64-linux/lib/:"$LD_LIBRARY_PATH"
+PATH=/usr/local/cuda/bin/:"$PATH" 
+
+# JOB STEPS (example: write hostname to output file, and wait 1 minute)
+#srun echo $CUDA_VISIBLE_DEVICES
+srun python code/main.py --config configs/arithmetic__div_hard.yaml
+
+
---
+++ evaluate_mul_div_multiple_hard.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --job-name=arithmetic_mul_div_multiple
+#SBATCH --output=train_logs/arithmetic_mul_div_multiple_out.txt
+#SBATCH --ntasks=1
+#SBATCH --mem=64G
+#SBATCH --partition=single
+#SBATCH --time=2-0:00:00
+#SBATCH -e train_logs/arithmetic_mul_div_multiple_error.txt
+# Add ICL-Slurm binaries to path
+PATH=/opt/slurm/bin:"$PATH"
+LD_LIBRARY_PATH=/usr/local/cuda/targets/x86_64-linux/lib/:"$LD_LIBRARY_PATH"
+PATH=/usr/local/cuda/bin/:"$PATH" 
+
+# JOB STEPS (example: write hostname to output file, and wait 1 minute)
+#srun echo $CUDA_VISIBLE_DEVICES
+srun python code/main.py --config configs/arithmetic__mul_div_multiple_hard.yaml
+
+
